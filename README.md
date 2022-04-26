b938c5cea56ae143205152e0ef903b97f805528d
Muiz Nadeem <32574315+MuizNadeem@users.noreply.github.com>
2022-04-27 00:50:11 +0530

    Kte token (#2)
    * vesting entites and mapping
    
    * add handle release vesting
    
    * handle upfront release
    
    * deployment steps
    
    * add access control subgraph
    
    * kte token graph
    
    Co-authored-by: Rachit Magon <rachit.magon@gmail.com>

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
 
-## Steps
+## Steps for running in Dev mode
 
-1. Deploy smart contract to index and specify the contract address in subgraph.yaml file.
+1. Specify the contract address of the locally deployed Smart Contract, the emitted Events and their handlers in `subgraph.template.yaml` file.
 
-2. Start a graph-node instance with `docker-compose up`
+2. `npm run codegen` whenever there are changes in `schema.graphql` file or the Contract abi(abis/*.json)
 
-3. `npm run codegen` whenever there are changes in `schema.graphql` file or the Contract abi(abis/*.json)
+3. `npm run prepare:local` to create `subgraph.yaml` file.
 
-4. `npm run deploy-local` to deploy event handler mappings to the graph node.
+4. Start a graph-node instance with `docker-compose up`
+
+5. `npm run create-local` to create an instance of the graph (run only first time. Subsequent Deployments are done on the same graph node instance)
+
+6. `npm run deploy-local` to deploy event handler mappings to the graph node.
+
+
+
+## Deployment Steps
+
+
+sign up on the graph with a github account
+create a graph project
+
+
+1. `npm install -g @graphprotocol/graph-cli`
+
+2. `graph auth --product hosted-service <ACCESS_TOKEN>`
+
+npm run codegen
+
+set contract address and startblock in <network>.json
+
+set repository address (<gitUserName>/<graphProjectName>) in subgraph.template.yaml and package.json
+
+3. `npm run deploy`
--- src/vesting-mapping.ts
+++ src/vesting-mapping.ts
@@ -1,17 +1,20 @@
-import { VestingSchedule } from '../generated/schema';
-import { AddVestingSchedule } from '../generated/TokenVesting/TokenVesting';
-import { TOKEN_VESTING_ADDRESS } from './constant';
-import { log } from '@graphprotocol/graph-ts';
+import { Claim, User, VestingSchedule } from '../generated/schema';
+import { AddVestingSchedule, ReleaseVestedToken, UpfrontTokenTransfer } from '../generated/TokenVesting/TokenVesting';
 
 export function handleAddVestingSchedule(event: AddVestingSchedule): void {
-  let vestingSchedule = VestingSchedule.load(TOKEN_VESTING_ADDRESS);
-  if (!vestingSchedule) {
-    vestingSchedule = new VestingSchedule(TOKEN_VESTING_ADDRESS);
+  // Create a new vesting schedule
+  let vestingScheduleId = event.params.vestingScheduleId.toHexString();
+  let vestingSchedule = new VestingSchedule(vestingScheduleId);
+  // Load or create new user
+  let userId = event.params.beneficiary.toHexString();
+  let user = User.load(userId);
+  if (!user) {
+    user = new User(userId);
   }
 
-  log.info('\n\n\n\n Schedule Id {}', [event.params.vestingScheduleId.toHexString()]);
-  vestingSchedule.id = event.params.vestingScheduleId.toHexString();
-  vestingSchedule.beneficiary = event.params.beneficiary;
+  vestingSchedule.id = vestingScheduleId;
+  vestingSchedule.beneficiary = userId;
+
   vestingSchedule.cliff = event.params.cliff.toI32();
   vestingSchedule.start = event.params.start.toI32();
   vestingSchedule.duration = event.params.duration.toI32();
@@ -21,5 +24,73 @@ export function handleAddVestingSchedule(event: AddVestingSchedule): void {
   vestingSchedule.released = event.params.released;
   vestingSchedule.revoked = event.params.revoked;
   vestingSchedule.upFront = event.params.upFront;
+
+  user.totalAllocation = user.totalAllocation.plus(vestingSchedule.amountTotal);
+
+  user.save();
   vestingSchedule.save();
+}
+
+export function handleReleaseVestedToken(event: ReleaseVestedToken): void {
+  let vestingScheduleId = event.params.vestingScheduleId.toHexString();
+  let userId = event.params.beneficiary.toHexString();
+  let amount = event.params.amount;
+  let vestingSchedule = VestingSchedule.load(vestingScheduleId);
+  if (!vestingSchedule) {
+    return;
+  }
+
+  let user = User.load(userId);
+  if (!user) {
+    return;
+  }
+
+  //Extract to common function
+  let claim = new Claim(event.transaction.hash.toHexString());
+  claim.amount = amount;
+  claim.beneficiary = userId;
+  claim.vestingSchedule = vestingScheduleId;
+  claim.timestamp = event.block.timestamp.toI32();
+  claim.claimType = 'PostVesting';
+
+  vestingSchedule.released = vestingSchedule.released.plus(amount);
+  user.totalReleased = user.totalReleased.plus(amount);
+
+  user.save();
+  vestingSchedule.save();
+  claim.save();
+}
+
+export function handleUpfrontTokenTransfer(event: UpfrontTokenTransfer): void {
+  let vestingScheduleId = event.params.vestingScheduleId.toHexString();
+  let amount = event.params.amount;
+
+  // Load or create new user
+  let userId = event.params.beneficiary.toHexString();
+  let user = User.load(userId);
+  if (!user) {
+    user = new User(userId);
+  }
+
+  // Load or create vesting
+  let vestingId = event.params.vestingScheduleId.toHexString();
+  let vesting = VestingSchedule.load(vestingId);
+  if (!vesting) {
+    vesting = new VestingSchedule(vestingId);
+  }
+
+  //Extract to common function
+  let claim = new Claim(event.transaction.hash.toHexString());
+  claim.amount = amount;
+  claim.beneficiary = userId;
+  claim.vestingSchedule = vestingScheduleId;
+  claim.timestamp = event.block.timestamp.toI32();
+  claim.claimType = 'UpFront';
+
+  vesting.released = vesting.released.plus(amount);
+  user.totalReleased = user.totalReleased.plus(amount);
+
+  user.save();
+  vesting.save();
+  claim.save();
 }
